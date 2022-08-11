b10b6765f74311d679fc38991bc232ef038c7ee5
tnaga <hqf00342@nifty.com>
2022-08-11 17:43:53 +0900

    Navigation関連のファイル分割

--- Form1.Navigation.cs
+++ Form1.Navigation.cs
@@ -1,136 +1,252 @@
-﻿/****************************************************************************
-Form1.cs
-ナビゲーション関連
-****************************************************************************/
-
+﻿using System;
 using System.Diagnostics;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Marmi;
+/*
+画面遷移関連のメソッド
+*/
 
-public partial class Form1
+namespace Marmi
 {
-    private void NavigateToBack()
+    public partial class Form1 : Form
     {
-        //前に戻る
-        int prev = GetPrevPageIndex(App.CurrentPage);
-        if (prev >= 0)
-            SetViewPage(prev);
-        else
-            _clearPanel.ShowAndClose("先頭のページです", 1000);
-    }
 
-    private void NavigateToForword()
-    {
-        //ver1.35 ループ機能を実装
-        int now = App.CurrentPage;
-        int next = GetNextPageIndex(App.CurrentPage);
-        Debug.WriteLine($"NavigateToForword() {now} -> {next}");
-        if (next >= 0)
+        /// <summary>
+        /// 指定したインデックスの画像を表示する。
+        /// publicになっている理由はサイドバーやサムネイル画面からの
+        /// 呼び出しに対応するため。
+        /// 前のページに戻らないようにdrawOrderTickを導入
+        /// </summary>
+        /// <param name="index">インデックス番号</param>
+        /// <param name="drawOrderTick">描写順序を示すオーダー時間 = DateTime.Now.Ticks</param>
+        public async Task SetViewPageAsync(int index, long drawOrderTick = 0)
         {
-            SetViewPage(next);
+            if (drawOrderTick == 0)
+                drawOrderTick = DateTime.Now.Ticks;
+
+            //ver1.09 オプションダイアログを閉じると必ずここに来ることに対するチェック
+            if (App.g_pi.Items == null || App.g_pi.Items.Count == 0)
+                return;
+
+            //ver1.36 Index範囲チェック
+            Debug.Assert(index >= 0 && index < App.g_pi.Items.Count);
+
+            // ページ進行方向 進む方向なら正、戻る方向なら負
+            // アニメーションで利用する
+            int pageDirection = index - App.g_pi.NowViewPage;
+
+            //ページ番号を更新
+            App.g_pi.NowViewPage = index;
+            _trackbar.Value = index;
+
+            //ver1.35 スクリーンキャッシュチェック
+            if (ScreenCache.Dic.TryGetValue(index, out Bitmap screenImage))
+            {
+                //スクリーンキャッシュあったのですぐに描写
+                SetViewPage2(index, pageDirection, screenImage, drawOrderTick);
+                Debug.WriteLine(index, "Use ScreenCache");
+            }
+            else
+            {
+                //ver1.50
+                //Keyだけある{key,null}キャッシュだったら消す。稀に発生するため
+                if (ScreenCache.Dic.ContainsKey(index))
+                    ScreenCache.Remove(index);
+
+                //ver1.50 読み込み中と表示
+                SetStatusbarInfo("Now Loading ... " + (index + 1).ToString());
+
+                //画像作成をスレッドプールに登録
+                var img = await Bmp.MakeOriginalSizeImageAsync(index);
+                if (img == null)
+                {
+                    PicPanel.Message = "読込みに時間がかかってます.リロードしてください";
+                }
+                else
+                {
+                    SetViewPage2(index, pageDirection, img, drawOrderTick);
+                }
+
+                //カーソルをWaitに
+                //this.Cursor = Cursors.WaitCursor;
+            }
+
+            //回転情報を適用
+            var rotate = App.g_pi.Items[App.g_pi.NowViewPage].Rotate;
+            if (rotate != 0)
+            {
+                PicPanel.Rotate(rotate);
+            }
         }
-        else if (App.Config.Draw.LastPage_toTop)
+
+        private void SetViewPage2(int index, int pageDirection, Bitmap screenImage, long orderTime)
         {
-            //先頭ページへループ
-            SetViewPage(0);
-            _clearPanel.ShowAndClose("先頭ページに戻りました", 1000);
+            //ver1.55 drawOrderTickのチェック.
+            // スレッドプールに入るため稀に順序が前後する。
+            // 最新の描写でなければスキップ
+            if (PicPanel.DrawOrderTime > orderTime)
+            {
+                Debug.WriteLine($"Skip SetViewPage2({index}) too old order={orderTime} < now={PicPanel.DrawOrderTime}");
+                return;
+            }
+
+            //描写開始
+            PicPanel.State = DrawStatus.drawing;
+            PicPanel.DrawOrderTime = orderTime;
+
+            if (screenImage == null)
+            {
+                Debug.WriteLine($"bmpがnull(index={index})");
+                PicPanel.State = DrawStatus.idle;
+                PicPanel.Message = "表示エラー 再度表示してみてください" + index.ToString();
+                PicPanel.Refresh();
+                return;
+            }
+
+            //ver1.50 表示
+            PicPanel.State = DrawStatus.drawing;
+            PicPanel.Message = string.Empty;
+            if (App.Config.View.PictureSwitchMode != AnimateMode.none  //アニメーションモードである
+                && !App.Config.KeepMagnification                  //倍率固定モードではアニメーションしない
+                && pageDirection != 0)
+            {
+                //スライドインアニメーション
+                PicPanel.AnimateSlideIn(screenImage, pageDirection);
+            }
+
+            PicPanel.Bmp = screenImage;
+            PicPanel.ResetView();
+            PicPanel.FastDraw = false;
+
+            //ver1.78 倍率をオプション指定できるように変更
+            if (!App.Config.KeepMagnification     //倍率維持モードではない
+                || IsFitToScreen)             //画面にフィットしている
+            {
+                //画面切り替わり時はフィットモードで起動
+                float r = PicPanel.FittingRatio;
+                if (r > 1.0f && App.Config.View.NoEnlargeOver100p)
+                    r = 1.0f;
+                PicPanel.ZoomRatio = r;
+            }
+
+            //ページを描写
+            PicPanel.AjustViewAndShow();
+
+            //1ページ表示か2ページ表示か
+            g_viewPages = (int)screenImage.Tag;
+            PicPanel.State = DrawStatus.idle;
+
+            //カーソルを元に戻す
+            this.Cursor = Cursors.Default;
+
+            //UI更新
+            UpdateStatusbar();
+            UpdateToolbar();
+
+            //サイドバーでアイテムを中心に
+            if (_sidebar.Visible)
+                _sidebar.SetItemToCenter(App.g_pi.NowViewPage);
+
+            needMakeScreenCache = true;
+
+            //PicPanel.Message = string.Empty;
+            PicPanel.State = DrawStatus.idle;
+            //2021年2月26日 GCをやめる
+            //ToDo:ここだけはあったほうがいいかもしれないがLOHの扱いも同時にすべき
+            //Uty.ForceGC();
         }
-        //2022年5月20日 次の書庫へオプションを廃止
-        //else if (App.Config.Draw.LastPage_toNextArchive)
-        //{
-        //    //ver1.70 最終ページで次の書庫を開く
-        //    if (App.g_pi.PackType != PackageType.Directory)
-        //    {
-        //        //次の書庫を探す
-        //        string filename = App.g_pi.PackageName;
-        //        string dirname = Path.GetDirectoryName(filename);
-        //        string[] files = Directory.GetFiles(dirname);
 
-        //        //ファイル名でソートする
-        //        //Array.Sort(files, Uty.Compare_unsafeFast);
-        //        Array.Sort(files, NaturalStringComparer.CompareS);
+        #region Navigation
 
-        //        bool match = false;
-        //        foreach (var s in files)
-        //        {
-        //            if (s == filename)
-        //            {
-        //                match = true;
-        //                continue;
-        //            }
-        //            if (match)
-        //            {
-        //                if (Uty.IsAvailableFile(s))
-        //                {
-        //                    _clearPanel.ShowAndClose("次へ移動します：" + Path.GetFileName(s), 1000);
-        //                    Start(new string[] { s });
-        //                    return;
-        //                }
-        //            }
-        //        }
-        //        _clearPanel.ShowAndClose("最後のページです。次の書庫が見つかりませんでした", 1000);
-        //    }
-        //    else
-        //    {
-        //        //先頭ページへループ
-        //        SetViewPage(0);
-        //        _clearPanel.ShowAndClose("先頭ページに戻りました", 1000);
-        //    }
-        //}
-        else //if(App.Config.lastPage_stay)
+        private async Task NavigateToBackAsync()
         {
-            _clearPanel.ShowAndClose("最後のページです", 1000);
+            //前に戻る
+            long drawOrderTick = DateTime.Now.Ticks;
+            int prev = await GetPrevPageIndex(App.g_pi.NowViewPage);
+            if (prev >= 0)
+            {
+                await SetViewPageAsync(prev, drawOrderTick);
+            }
+            else
+                _clearPanel.ShowAndClose("先頭のページです", 1000);
         }
-    }
 
-    /// <summary>
-    /// 最終ページを見ているかどうか確認。２ページ表示に対応
-    /// 先頭ページはそのまま０かどうかチェックするだけなので作成しない。
-    /// </summary>
-    /// <returns>最終ページであればtrue</returns>
-    private static bool IsLastPageViewing()
-    {
-        if (string.IsNullOrEmpty(App.g_pi.PackageName))
-            return false;
-        if (App.TotalPages <= 1)
-            return false;
-        return App.CurrentPage + App.CurrentShowPages >= App.TotalPages;
-    }
-
-    //ver1.35 前のページ番号。すでに先頭ページなら-1
-    private static int GetPrevPageIndex(int index)
-    {
-        if (index > 0)
+        private async Task NavigateToForwordAsync()
         {
-            int declimentPages = -1;
-            //2ページ減らすことが出来るか
-            if (CanDualView(App.CurrentPage - 2))
-                declimentPages = -2;
+            //ver1.35 ループ機能を実装
+            long drawOrderTick = DateTime.Now.Ticks;
+            int now = App.g_pi.NowViewPage;
+            int next = await GetNextPageIndexAsync(App.g_pi.NowViewPage);
+            Debug.WriteLine($"NavigateToForword() {now} -> {next}");
+            if (next >= 0)
+            {
+                await SetViewPageAsync(next, drawOrderTick);
+            }
+            else if (App.Config.View.LastPage_toTop)
+            {
+                //先頭ページへループ
+                await SetViewPageAsync(0, drawOrderTick);
+                _clearPanel.ShowAndClose("先頭ページに戻りました", 1000);
+            }
+            else
+            {
+                _clearPanel.ShowAndClose("最後のページです", 1000);
+            }
+        }
 
-            int ret = index + declimentPages;
-            return ret >= 0 ? ret : 0;
-        }
-        else
+        /// <summary>
+        /// 最終ページを見ているかどうか確認。２ページ表示に対応
+        /// 先頭ページはそのまま０かどうかチェックするだけなので作成しない。
+        /// </summary>
+        /// <returns>最終ページであればtrue</returns>
+        private bool IsLastPageViewing()
         {
-            //すでに先頭ページなので-1を返す
-            return -1;
+            if (string.IsNullOrEmpty(App.g_pi.PackageName))
+                return false;
+            if (App.g_pi.Items.Count <= 1)
+                return false;
+            return App.g_pi.NowViewPage + g_viewPages >= App.g_pi.Items.Count;
         }
-    }
 
-    //ver1.36次のページ番号。すでに最終ページなら-1
-    private static int GetNextPageIndex(int index)
-    {
-        //int pages = CanDualView(index) ? 2 : 1;
-        int pages = App.CurrentShowPages;
+        //ver1.35 前のページ番号。すでに先頭ページなら-1
+        internal static async Task<int> GetPrevPageIndex(int index)
+        {
+            if (index > 0)
+            {
+                int declimentPages = -1;
+                //2ページ減らすことが出来るか
+                if (await CanDualViewAsync(App.g_pi.NowViewPage - 2))
+                    declimentPages = -2;
 
-        if (index + pages <= App.TotalPages - 1)
-        {
-            return index + pages;
+                int ret = index + declimentPages;
+                return ret >= 0 ? ret : 0;
+            }
+            else
+            {
+                //すでに先頭ページなので-1を返す
+                return -1;
+            }
         }
-        else
+
+        //ver1.36次のページ番号。すでに最終ページなら-1
+        internal static async Task<int> GetNextPageIndexAsync(int index)
         {
-            //最終ページ
-            return -1;
+            int pages = await CanDualViewAsync(index) ? 2 : 1;
+
+            if (index + pages <= App.g_pi.Items.Count - 1)
+            {
+                return index + pages;
+            }
+            else
+            {
+                //最終ページ
+                return -1;
+            }
         }
+
+        #endregion Navigation
+
     }
 }
