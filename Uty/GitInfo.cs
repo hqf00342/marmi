7e214d38aa6ef2db97feb4e096c3b451268c3a30
tnaga <hqf00342@nifty.com>
2022-11-28 16:50:04 +0900

    GitInfoを導入

---
+++ Uty/GitInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+
+internal static class GitInfo
+{
+    public static string CommitId => GetResouce("git_id.txt");
+    public static string BranchName => GetResouce("git_branch.txt");
+
+    private static string GetResouce(string name)
+    {
+        var asm = Assembly.GetExecutingAssembly();
+        var resName = asm.GetManifestResourceNames().FirstOrDefault(a => a.EndsWith(name));
+        if (resName == null) return string.Empty;
+
+        using (var st = asm.GetManifestResourceStream(resName))
+        {
+            if (st == null) return string.Empty;
+            var reader = new StreamReader(st);
+            return reader.ReadToEnd().Trim('\r', '\n');
+        }
+    }
+}
