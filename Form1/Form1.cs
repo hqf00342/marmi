3bb325c6368aea3fdc829460a6aac2cabd1be775
tnaga <hqf00342@nifty.com>
2022-11-30 23:17:20 +0900

    IPC機能をファイル分離

--- Form1/Form1.cs
+++ Form1/Form1.cs
@@ -767,32 +767,6 @@ namespace Marmi
 
         #endregion スライドショー
 
-        /// <summary>
-        /// IPCで呼ばれるインターフェースメソッド
-        /// コマンドライン引数が入ってくるのでそれを起動。
-        /// ただし、このメソッドが呼ばれるときはフォームのスレッドではないので
-        /// Invokeが必要
-        /// </summary>
-        /// <param name="args">コマンドライン引数</param>
-        void IRemoteObject.IPCMessage(string[] args)
-        {
-            this.Invoke((Action)(() =>
-            {
-                //自分を前面にする
-                this.Activate();
-
-                //表示対象ファイルを取得
-                //1つめに自分のexeファイル名が入っているので除く
-                if (args.Length > 1)
-                {
-                    //AsyncStart(args.Skip(1).ToArray());
-#pragma warning disable CS4014 // この呼び出しは待機されなかったため、現在のメソッドの実行は呼び出しの完了を待たずに続行されます
-                    StartAsync(args.Skip(1).ToArray());
-#pragma warning restore CS4014 // この呼び出しは待機されなかったため、現在のメソッドの実行は呼び出しの完了を待たずに続行されます
-                }
-            }));
-        }
-
         protected override void OnActivated(EventArgs e)
         {
             base.OnActivated(e);
