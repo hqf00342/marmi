afff904dcc0c54496d89562aca77e19fef752a91
tnaga <hqf00342@nifty.com>
2023-01-22 13:29:08 +0900

    コメント追加。100%オーバー表示をデフォルトに戻した。

--- DataModel/Configs/ViewConfig.cs
+++ DataModel/Configs/ViewConfig.cs
@@ -2,21 +2,25 @@
 {
     public class ViewConfig
     {
-        //画像サイズ調整は100%未満にする
+        /// <summary>画像サイズ調整は100%未満にする</summary>
         public bool ProhigitExpansionOver100p { get; set; }
 
-        //Dot-by-Dot補間モードにする(100%以上に拡大する際に補完しない)
+        /// <summary>Dot-by-Dot補間モード(1拡大時に補完しない)</summary>
         public bool DotByDotZoom { get; set; }
 
-        //ver1.79 2ページモード
+        /// <summary>2ページモード：強制</summary>
         public bool DualView_Force { get; set; }
 
+        /// <summary>2ページモード：通常（縦長画像同士）</summary>
         public bool DualView_Normal { get; set; }
+
+        /// <summary>2ページモード：サイズチェック（縦長＋高さが等しい）</summary>
         public bool DualView_withSizeCheck { get; set; }
 
-        //ver1.71 最終ページの動作
+        /// <summary>最終ページに留まる</summary>
         public bool StayOnLastPage { get; set; }
 
+        /// <summary>最終ページに留まる</summary>
         public bool MoveToTopAtLastPage { get; set; }
 
         //ver1.21画像切り替え方法
@@ -24,7 +28,7 @@
 
         public void Init()
         {
-            ProhigitExpansionOver100p = true;
+            ProhigitExpansionOver100p = false;
             DotByDotZoom = false;
             StayOnLastPage = true;
             MoveToTopAtLastPage = false;
