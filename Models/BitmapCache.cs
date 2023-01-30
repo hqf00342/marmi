2b4e6f3b68a14a6df9147a52e90b0ce83ef32f9a
tnaga <hqf00342@nifty.com>
2023-01-30 16:00:23 +0900

    BitmapCacheでのGC.Collect()を中止

--- Models/BitmapCache.cs
+++ Models/BitmapCache.cs
@@ -8,6 +8,8 @@ Bitmapをキャッシュするクラス
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 
 namespace Marmi.Models
@@ -19,12 +21,11 @@ namespace Marmi.Models
         public void ClearAll()
         {
             _list.Clear();
-            GC.Collect();
         }
 
         public Bitmap GetBitmap(int index, CacheTag tag)
         {
-            var item = _list.FirstOrDefault(x => x.Index==index && x.Tag == tag);
+            var item = _list.FirstOrDefault(x => x.Index == index && x.Tag == tag);
             if (item == null)
             {
                 //見つからないときはnullを返す。
@@ -33,7 +34,7 @@ namespace Marmi.Models
             else
             {
                 item.ReferenceCount++;
-                item.LastReferTime= DateTime.Now;
+                item.LastReferTime = DateTime.Now;
                 return item.Bitmap;
             }
         }
@@ -43,26 +44,34 @@ namespace Marmi.Models
             var exists = _list.Any(x => x.Index == index && x.Tag == tag);
             if (exists) return false;
 
-            _list.Add(new TagBitmap(index,tag,bitmap));
+            _list.Add(new TagBitmap(index, tag, bitmap));
             return true;
         }
 
-        public void  Clear(CacheTag Tag)
+        public void Clear(CacheTag Tag)
         {
             _list.RemoveAll(x => x.Tag == Tag);
-            GC.Collect();
         }
 
         public void Clear(int index)
         {
-            _list.RemoveAll(x=>x.Index==index);
-            GC.Collect();
+            _list.RemoveAll(x => x.Index == index);
         }
 
         private class TagBitmap
         {
+            private byte[] _rawImage = null;
+
             public Bitmap Bitmap { get; set; }
+
+            //public Bitmap Bitmap
+            //{
+            //    get=>GetBitmapFromRawImage();
+            //    set=>Store(value);
+            //}
+
             public int Index { get; set; }
+
             public CacheTag Tag { get; set; }
 
             public DateTime CreateTime { get; set; }
@@ -73,13 +82,40 @@ namespace Marmi.Models
 
             public TagBitmap(int index, CacheTag tag, Bitmap bmp)
             {
-                Bitmap= bmp;
-                Index= index;
-                Tag= tag;
-                CreateTime= DateTime.Now;
-                LastReferTime= DateTime.Now;
+                Bitmap = bmp;
+                Index = index;
+                Tag = tag;
+                CreateTime = DateTime.Now;
+                LastReferTime = DateTime.Now;
+            }
+
+            private void Store(Bitmap bmp)
+            {
+                if (bmp != null)
+                {
+                    //pngで保存
+                    var ms = new MemoryStream();
+                    bmp.Save(ms, ImageFormat.Png);
+                    ms.Close();
+                    _rawImage = ms.ToArray();
+                }
+            }
+
+            private Bitmap GetBitmapFromRawImage()
+            {
+                if (_rawImage != null)
+                {
+                    try
+                    {
+                        var ic = new ImageConverter();
+                        return ic.ConvertFrom(_rawImage) as Bitmap;
+                    }
+                    catch
+                    {
+                    }
+                }
+                return null;
             }
         }
     }
-
 }
